4205ec5bf3e436ffcd37ea86431db680c50187c9
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-02-07 14:34:36 +0000

    best_weight support

--- run_ssd.sh
+++ run_ssd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd "$1" ; ./bin/edged.py $2 ; python3 predict.py weights/close_weight_1.2027226681531218.pth ./edged.jpg "$2" ; cp result.jpg ~/game_eye
+cd "$1" ; ./bin/edged.py $2 ; python3 predict.py weights/best_weight.pth ./edged.jpg "$2" ; cp result.jpg ~/game_eye
